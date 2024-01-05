ad01622a2a8ef60ca9f19509bf855a6ae7f96398
David <davidmchung123@gmail.com>
2024-01-05 10:22:26 -0600

    uncommitted file

--- src/main/java/com/example/virtuallibrary/repository/BookRepository.java
+++ src/main/java/com/example/virtuallibrary/repository/BookRepository.java
@@ -1,8 +1,10 @@
-package com.example.virtuallibrary.models;
+package com.example.virtuallibrary.repository;
 
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+
+import com.example.virtuallibrary.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
