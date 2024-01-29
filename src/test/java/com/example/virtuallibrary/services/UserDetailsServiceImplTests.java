d09d7c34574d20c56b04f8df843d0b00dfe434c8
David <davidmchung123@gmail.com>
2024-01-29 11:43:58 -0600

    Fixed minor error.

--- src/test/java/com/example/virtuallibrary/services/UserDetailsServiceImplTests.java
+++ src/test/java/com/example/virtuallibrary/services/UserDetailsServiceImplTests.java
@@ -72,4 +72,5 @@ public class UserDetailsServiceImplTests {
         assertThrows(UserNotFoundException.class, () -> {    
             userDetailsService.findUserById(mockId);
         });
+    }
 }
